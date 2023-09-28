f34ebb9033a302948d2a27fe0f57ccbdf39446e4
ingaleniranjan365 <ingaleniranjan365@gmail.com>
2023-09-28 17:02:41 +0530

    Adding spark job for airport insights - computes taxi trips per hour_of_day to NYC airports

--- nyc-taxi-trip-duration-pyspark/terraform/main.tf
+++ nyc-taxi-trip-duration-pyspark/terraform/main.tf
@@ -9,22 +9,34 @@ resource "google_storage_bucket" "dataproc_bucket" {
   location = "US"
 }
 
-resource "google_storage_bucket_object" "predict_trip_duration_pyspark_file" {
-  name   = "nyc_taxi_trip_experiments/predict_trip_duration.py"
+resource "google_storage_bucket_object" "insights_pyspark_file" {
+  name   = "nyc_taxi_trip_experiments/insights/airport_insights.py"
   bucket = google_storage_bucket.dataproc_bucket.name
-  source = "../predict_trip_duration.py"
+  source = "../airport_insights.py"
 }
 
-resource "google_storage_bucket_object" "predict_trip_duration_training_trips" {
-  name   = "nyc_taxi_trip_experiments/train.csv"
+resource "google_storage_bucket_object" "ride_hailing_services_taxi_trips" {
+  name   = "nyc_taxi_trip_experiments/nyc-tlc-raw-taxi-trips/fhvhv_tripdata_2023-06.parquet"
   bucket = google_storage_bucket.dataproc_bucket.name
-  source = "../nyc-taxi-trip-duration/train.csv"
+  source = "../nyc-tlc-raw-taxi-trips/fhvhv_tripdata_2023-06.parquet"
 }
 
-resource "google_storage_bucket_object" "predict_trip_duration_testing_trips" {
-  name   = "nyc_taxi_trip_experiments/test.csv"
+resource "google_storage_bucket_object" "green_taxi_trips" {
+  name   = "nyc_taxi_trip_experiments/nyc-tlc-raw-taxi-trips/green_tripdata_2023-06.parquet"
   bucket = google_storage_bucket.dataproc_bucket.name
-  source = "../nyc-taxi-trip-duration/test.csv"
+  source = "../nyc-tlc-raw-taxi-trips/green_tripdata_2023-06.parquet"
+}
+
+resource "google_storage_bucket_object" "yellow_taxi_trips" {
+  name   = "nyc_taxi_trip_experiments/nyc-tlc-raw-taxi-trips/yellow_tripdata_2023-06.parquet"
+  bucket = google_storage_bucket.dataproc_bucket.name
+  source = "../nyc-tlc-raw-taxi-trips/yellow_tripdata_2023-06.parquet"
+}
+
+resource "google_storage_bucket_object" "taxi_zones_lookup_table" {
+  name   = "nyc_taxi_trip_experiments/nyc-tlc-raw-taxi-trips/taxi+_zone_lookup.csv"
+  bucket = google_storage_bucket.dataproc_bucket.name
+  source = "../nyc-tlc-raw-taxi-trips/taxi+_zone_lookup.csv"
 }
 
 resource "google_service_account" "dataproc_sa" {
@@ -54,8 +66,8 @@ resource "google_project_iam_member" "service_account_roles" {
   member = "serviceAccount:${google_service_account.dataproc_sa.email}"
 }
 
-resource "google_dataproc_cluster" "nyc_trip_duration_prediction" {
-  name     = "nyc-trip-duration-prediction"
+resource "google_dataproc_cluster" "nyc_taxi_trip_experiments" {
+  name     = "nyc-taxi-trip-experiments"
   region   = "us-central1"
   graceful_decommission_timeout = "120s"
   labels = {
@@ -78,7 +90,6 @@ resource "google_dataproc_cluster" "nyc_trip_duration_prediction" {
       }
     }
 
-    # Override or set some custom properties
     software_config {
       image_version = "2.0-debian10"
       override_properties = {
@@ -88,43 +99,80 @@ resource "google_dataproc_cluster" "nyc_trip_duration_prediction" {
 
     gce_cluster_config {
       tags = ["foo", "bar"]
-      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
       service_account = google_service_account.dataproc_sa.email
       service_account_scopes = [
         "cloud-platform"
       ]
     }
-
-    # You can define multiple initialization_action blocks
-#    initialization_action {
-#      script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
-#      timeout_sec = 500
-#    }
   }
 }
 
-resource "google_dataproc_job" "pyspark_nyc_taxi_trip_duration_prediction" {
-  region       = google_dataproc_cluster.nyc_trip_duration_prediction.region
+resource "google_dataproc_job" "airport_insights" {
+  region       = google_dataproc_cluster.nyc_taxi_trip_experiments.region
   force_delete = true
   placement {
-    cluster_name = google_dataproc_cluster.nyc_trip_duration_prediction.name
+    cluster_name = google_dataproc_cluster.nyc_taxi_trip_experiments.name
   }
 
   pyspark_config {
-    main_python_file_uri = "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.predict_trip_duration_pyspark_file.name}"
+    main_python_file_uri = "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.insights_pyspark_file.name}"
     properties = {
       "spark.logConf" = "true"
     }
 
     args = [
-      "gs://nyc_taxi_trip_duration_dataproc_bucket/nyc_taxi_trip_experiments/train.csv",
-      "gs://nyc_taxi_trip_duration_dataproc_bucket/nyc_taxi_trip_experiments/test.csv",
-      "gs://nyc_taxi_trip_duration_dataproc_bucket/nyc_taxi_trip_experiments/predictions",
+      "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.ride_hailing_services_taxi_trips.name}",
+      "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.green_taxi_trips.name}",
+      "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.yellow_taxi_trips.name}",
+      "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.taxi_zones_lookup_table.name}",
+      "gs://${google_storage_bucket.dataproc_bucket.name}/nyc_taxi_trip_experiments/insights/airport_insights"
     ]
 
   }
 }
 
 output "pyspark_status" {
-  value = google_dataproc_job.pyspark_nyc_taxi_trip_duration_prediction.status[0].state
+  value = google_dataproc_job.airport_insights.status[0].state
 }
+
+#
+#resource "google_storage_bucket_object" "predict_trip_duration_pyspark_file" {
+#  name   = "nyc_taxi_trip_experiments/predict_trip_duration.py"
+#  bucket = google_storage_bucket.dataproc_bucket.name
+#  source = "../predict_trip_duration.py"
+#}
+#
+#resource "google_storage_bucket_object" "predict_trip_duration_training_trips" {
+#  name   = "nyc_taxi_trip_experiments/train.csv"
+#  bucket = google_storage_bucket.dataproc_bucket.name
+#  source = "../nyc-taxi-trip-duration/train.csv"
+#}
+#
+#resource "google_storage_bucket_object" "predict_trip_duration_testing_trips" {
+#  name   = "nyc_taxi_trip_experiments/test.csv"
+#  bucket = google_storage_bucket.dataproc_bucket.name
+#  source = "../nyc-taxi-trip-duration/test.csv"
+#}
+#
+#resource "google_dataproc_job" "pyspark_nyc_taxi_trip_duration_prediction" {
+#  region       = google_dataproc_cluster.nyc_trip_duration_prediction.region
+#  force_delete = true
+#  placement {
+#    cluster_name = google_dataproc_cluster.nyc_trip_duration_prediction.name
+#  }
+#
+#  pyspark_config {
+#    main_python_file_uri = "gs://${google_storage_bucket.dataproc_bucket.name}/${google_storage_bucket_object.predict_trip_duration_pyspark_file.name}"
+#    properties = {
+#      "spark.logConf" = "true"
+#    }
+#
+#    args = [
+#      "gs://nyc_taxi_trip_duration_dataproc_bucket/nyc_taxi_trip_experiments/train.csv",
+#      "gs://nyc_taxi_trip_duration_dataproc_bucket/nyc_taxi_trip_experiments/test.csv",
+#      "gs://nyc_taxi_trip_duration_dataproc_bucket/nyc_taxi_trip_experiments/predictions",
+#    ]
+#
+#  }
+#}
+
